3d47b3c0935c35b185f718e99ea8c73ad357dede
Kamil <grigor884@gmail.com>
2017-10-11 12:28:30 +0200

    Create Testy.java

---
+++ Testy.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Testy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Witacz witaj = new Witacz();
+		witaj.witaj();
+		for(int i=0; i<10; i++)
+		System.out.println("Hello!!!");
+	}
+
+}
